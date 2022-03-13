f356f1c03032305373b87a87ee20de0a1fdafa1a
Kooky <kooky1001@gmail.com>
2022-03-13 12:42:46 +0900

    gitignore 적용

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -1,18 +1,16 @@
 var password = require('../password.js');
-
+var tunnel = require('tunnel-ssh');
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host : password.db.host,
-    user : password.db.user,
-    password : password.db.password,
-    database : password.db.database
+
+var ssh = tunnel(password.ssh, function(error, server){
+    if(error) throw error;
 });
+var connection = mysql.createConnection(password.db);
+    connection.connect();
 
-connection.connect();
-
-connection.query('select * from topic', function(error, results, fields) {
-    if (error) throw error;
-    console.log(results);
-});
-
-connection.end();
+    connection.query('select * from topic', function(error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+    });
+    connection.end();
+ssh.close();
