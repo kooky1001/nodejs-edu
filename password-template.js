4bc44de706a4f6a7872606c127619e854569c7bb
Kooky <kooky1001@gmail.com>
2022-03-13 17:33:47 +0900

    add password-template

---
+++ password-template.js
@@ -0,0 +1,18 @@
+var db = {
+  host : '',
+  user : '',
+  password : '',
+  database : ''
+}
+var ssh = {
+  username : '',
+  password : '',
+  privateKey : '',
+  host : '',
+  port : 0,
+  dstPort : 0
+}
+module.exports = {
+  db : db,
+  ssh : ssh
+}
