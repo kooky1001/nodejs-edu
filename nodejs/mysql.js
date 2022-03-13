c1ff54162863b3b3408be5fea325052bf34f5873
Kooky <kooky1001@gmail.com>
2022-03-13 12:52:20 +0900

    add ssh lib

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -1,16 +1,12 @@
 var password = require('../password.js');
-var tunnel = require('tunnel-ssh');
+var ssh = require('../lib/ssh');
 var mysql = require('mysql');
 
-var ssh = tunnel(password.ssh, function(error, server){
-    if(error) throw error;
-});
 var connection = mysql.createConnection(password.db);
-    connection.connect();
-
-    connection.query('select * from topic', function(error, results, fields) {
-        if (error) throw error;
-        console.log(results);
-    });
-    connection.end();
+connection.connect();
+connection.query('select * from topic', function(error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+});
+connection.end();
 ssh.close();
