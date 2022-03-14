8b31402c5c417b6652a2e195b707539c24065043
Kooky <kooky1001@gmail.com>
2022-03-14 23:31:25 +0900

    express로 코드정리

--- main.js
+++ main.js
@@ -1,45 +1,40 @@
 const express = require('express');
 const app = express();
-var ssh = require('./lib/ssh');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const ssh = require('./lib/ssh');
 const topic = require('./lib/topic');
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+
 app.get('/', function(req, res){
-  //res.send('test!!');
   topic.home(req, res);
 });
 
-app.listen(3000);
-
-/*
-var http = require('http');
-var url = require('url');
-var ssh = require('./lib/ssh');
-var topic = require('./lib/topic');
-
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-        topic.home(request,response);
-      } else {
-        topic.page(request,response);
-      }
-    } else if(pathname === '/create'){
-      topic.create(request,response);
-    } else if(pathname === '/create_process'){
-      topic.create_process(request,response);
-    } else if(pathname === '/update'){
-      topic.update(request,response);
-    } else if(pathname === '/update_process'){
-      topic.update_process(request, response);
-    } else if(pathname === '/delete_process'){
-      topic.delete_process(request, response);
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
+app.get('/page/:pageId', function(req, res){
+  topic.page(req, res);
 });
+
+app.get('/create', function(req, res){
+  topic.create(req, res);
+});
+
+app.post('/create_process', function(req, res){
+  topic.create_process(req, res);
+});
+
+app.get('/update/page/:pageId', function(req, res){
+  topic.update(req, res);
+});
+
+app.post('/update_process', function(req, res){
+  topic.update_process(req, res);
+});
+
+app.post('/delete_process', function(req, res){
+  topic.delete_process(req, res);
+});
+
 app.listen(3000);
-*/
