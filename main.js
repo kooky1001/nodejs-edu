0c431396c41405a531b4eae161f4935dda4ed3ed
Kooky <kooky1001@gmail.com>
2022-03-15 16:03:58 +0900

    topic route 분리

--- main.js
+++ main.js
@@ -3,38 +3,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const ssh = require('./lib/ssh');
-const topic = require('./lib/topic');
+
+// Routers
+var topicRouter = require('./routes/topics');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 
+app.use('/topic', topicRouter);
+
 app.get('/', function(req, res){
-  topic.home(req, res);
-});
-
-app.get('/page/:pageId', function(req, res){
-  topic.page(req, res);
-});
-
-app.get('/create', function(req, res){
-  topic.create(req, res);
-});
-
-app.post('/create_process', function(req, res){
-  topic.create_process(req, res);
-});
-
-app.get('/update/page/:pageId', function(req, res){
-  topic.update(req, res);
-});
-
-app.post('/update_process', function(req, res){
-  topic.update_process(req, res);
-});
-
-app.post('/delete_process', function(req, res){
-  topic.delete_process(req, res);
+  res.redirect('/topic');
 });
 
 app.listen(3000);
