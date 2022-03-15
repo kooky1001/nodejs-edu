d1e51754d58baf4a4d8d2641489fc2b364b07195
Kooky <kooky1001@gmail.com>
2022-03-15 16:04:10 +0900

    topic route 분리

---
+++ routes/topics.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+const topic = require('../lib/topic');
+
+router.get('/', function(req, res){
+  topic.home(req, res);
+});
+
+router.get('/create', function(req, res){
+  topic.create(req, res);
+});
+
+router.post('/create_process', function(req, res){
+  topic.create_process(req, res);
+});
+
+router.get('/update/:pageId', function(req, res){
+  topic.update(req, res);
+});
+
+router.post('/update_process', function(req, res){
+  topic.update_process(req, res);
+});
+
+router.post('/delete_process', function(req, res){
+  topic.delete_process(req, res);
+});
+
+router.get('/:pageId', function(req, res){
+  topic.page(req, res);
+});
+
+module.exports = router;
