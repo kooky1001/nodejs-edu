b17460b0a0d54c2973c25174f082a8ae1466608c
kooky1001 <kooky1001@gmail.com>
2025-10-10 13:44:08 +0900

    ssh 제거 sqlite 적용

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const compression = require('compression');
-const ssh = require('./lib/ssh');
 
 // Routers
 var topicRouter = require('./routes/topics');
