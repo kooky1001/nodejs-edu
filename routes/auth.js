f768e74deb80eb8c09e47d28d5d409cefa6ab93b
kooky1001 <kooky1001@gmail.com>
2025-10-10 17:04:50 +0900

    ejs 뷰템플릿 적용 / 리팩토링

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,8 @@
 var express = require('express');
-const template = require('../lib/template');
 var router = express.Router();
 
 router.get('/login', function(req, res){
-  var title = 'Web - login';
-  var html = template.HTML(title, null, `
-  <form action="/auth/login_process" method="post">
-  <p><input type="text" name="email" placeholder="email"></p>
-  <p><input type="password" name="pwd" placeholder="password"></p>
-  <p><input type="submit" value="login"></p>
-  </form>
-  `, '');
-  res.send(html);
+  res.render('auth/login');
 });
 /*
 router.get('/', function(req, res){
